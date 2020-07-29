17f1129057b16a573c1643dc88da3252e775a9ce
deneke@tropos.de <deneke@tropos.de>
2020-07-29 15:39:59 +0200

    Initial project version

---
+++ src/trosat/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of the TROPOS Satellite Suite (TROOSAT) developed 
+# by the satellite group at TROPOS.
+#
+# TROSAT is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors/Copyright(2012-2020):
+# *Hartwig Deneke (deneke@tropos.de)
+#
+# With various contributions by:
+# * Frank Werner (werner@tropos.de)
+# * Fabian Senf (senf@tropos.de)
+# * Sebastian Bley (bley@tropos.de)
+# * Jonas Witthuhn (witthuhn@tropos.de)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)

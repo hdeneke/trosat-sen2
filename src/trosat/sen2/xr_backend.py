1ff11134f3f13eeb0e4dceacce8e2ade1785a83b
hdeneke <deneke@tropos.de>
2024-10-21 22:02:48 +0200

    trosat.sen2.xr_backend.py: add l1c_store used for sen2_backend Xarray plugin

--- src/trosat/sen2/xr_backend.py
+++ src/trosat/sen2/xr_backend.py
@@ -4,11 +4,98 @@ import re
 import numpy as np
 import xarray as xr
 
+from xarray.core.utils import Frozen
 from xarray.backends.common import (
     BackendEntrypoint,
     BackendArray,
     AbstractDataStore
 )
+
+from osgeo import gdal
+
+from .core import l1c_reader, l1c_band
+
+class l1c_store(AbstractDataStore, l1c_reader):
+
+    def __init__(
+            self, 
+            fpath,
+            *, 
+            res = 60.0,
+            detfoo = False, 
+            qmask = False,
+            bands = ["B02","B03","B04"],
+    ):
+        l1c_reader.__init__(self, fpath)
+        self.res = res
+        #self.rs_up = gdal.GRA_Lanczos
+        #self.rs_dn = gdal.GRA_Average
+        self.detfoo = detfoo
+        self.qmask = qmask
+        #self.lazy = lazy
+        self.bands = bands
+        return
+
+    @classmethod
+    def open(cls, fpath, **kwargs):
+        return cls(fpath, **kwargs)
+
+    def get_attrs(self):
+        return Frozen(self.get_global_attrs())
+
+    def get_band_var(self, b):
+        b = l1c_band[b] if not isinstance(b, l1c_band) else b
+        arr  = self.read_band(
+            b, res=self.res, rs_algo=gdal.GRA_Bilinear
+        )
+        atts = self.get_band_attrs(b)
+        return xr.Variable(
+            dims=("y","x"),
+            data=arr,
+            attrs=atts,
+        )
+
+    def get_qmask_var(self, b):
+        arr  = self.read_qmask(b, res=self.res)
+        atts = {"comment": "band quality mask: "+b.lower()}
+        return xr.Variable(
+            dims=("y","x"),
+            data=arr,
+            attrs=atts,
+        )
+
+    def get_detfoo_var(self, b):
+        arr = self.read_detfoo(b, res=self.res)
+        atts = {"comment": "band quality mask: "+b.lower()}
+        return xr.Variable(
+            dims=("y","x"),
+            data=arr,
+            attrs=atts,
+        )
+
+    def get_variables(self):
+        vars = {}
+        for b in self.bands:
+            k = "refl_"+b.lower()
+            vars[k] = self.get_band_var(b)
+            if self.detfoo:
+                k = "detfoo_"+b.lower()
+                vars[k] = self.get_detfoo_var(b)
+            if self.qmask:
+                k = "qmask_"+b.lower()
+                vars[k] = self.get_qmask_var(b)
+        xax,yax = self.get_xy_axes(res=self.res, bounds=False)
+        vars["x"] = xr.Variable(
+            dims="x",
+            data=xax/1e3,
+            attrs={"standard_name":"projection_x_coordinate", "units":"km"},
+        )
+        vars["y"] = xr.Variable(
+            dims="y",
+            data=yax/1e3,
+            attrs={"standard_name":"projection_y_coordinate", "units":"km"},
+        )
+        return xr.core.utils.Frozen(vars)
 
 class sen2_backend(BackendEntrypoint):
 
@@ -28,17 +115,24 @@ class sen2_backend(BackendEntrypoint):
         self,
         fpath,
         *,
-        mask_and_scale=False,
+        #mask_and_scale=False,
         drop_variables=None,
-        decode_times=True,
-        decode_coords=True,
+        #decode_times=True,
+        #decode_coords=True,
         res=60.0,
         bands=["B02","B03","B04","B08","B11"],
-        lazy=False,
+        detfoo=False,
+        qmask=False,
     ):
 
         # build dataset using data store
-        with l1c_store(fpath, res=res, bands=bands) as store:
+        with l1c_store(
+                fpath,
+                res=res,
+                bands=bands,
+                detfoo=detfoo,
+                qmask=qmask
+        ) as store:
             # get attributes
             vars, atts = store.load()
             ds = xr.Dataset(vars, attrs=atts)
